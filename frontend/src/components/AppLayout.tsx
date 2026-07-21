48625d447d72aa686c3b3d7d1e93008ce97f1c7e
Nicholas Januar <nicho.januar@gmail.com>
2026-07-21 00:30:07 -0700

    scroll restoration

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -1,8 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { useLayoutEffect, useRef } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { PresenceBar } from './PresenceBar'
 import { CursorLayer } from './CursorLayer'
 
 export function AppLayout() {
+  const { pathname } = useLocation()
+  const scrollPositions = useRef<Record<string, number>>({})
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, scrollPositions.current[pathname] ?? 0)
+
+    return () => {
+      scrollPositions.current[pathname] = window.scrollY
+    }
+  }, [pathname])
+
   return (
     <>
       <PresenceBar />
